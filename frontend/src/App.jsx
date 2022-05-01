7ccdc7a935b886fbb42d36c69478cec2acf53039
WENDYBAQUE <BAQUE.WENDY@gmail.com>
2022-05-01 15:50:29 +0200

    Add About component and his style in about.css file and his route in app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Intro from "./components/Intro";
+import About from "./components/About";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Intro />
+      <About />
     </div>
   );
 }

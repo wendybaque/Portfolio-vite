4ee27ceb63297a14e549a6208653334edb4e5dce
WENDYBAQUE <BAQUE.WENDY@gmail.com>
2022-05-01 16:24:32 +0200

    Add image and his background and the left side of the about.jsx component

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 import "./about.css";
+import cv from "../assets/CVrecherchestage.png";
 
 function About() {
-  return <div className="about" />;
+  return (
+    <div className="about">
+      <div className="about-left">
+        left
+        <div className="about-card bg" />
+        <div className="about-card">
+          <img src={cv} alt="curriculum vitae" className="about-img" />
+        </div>
+      </div>
+      <div className="about-right">right</div>
+    </div>
+  );
 }
 export default About;

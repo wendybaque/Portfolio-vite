19b1c78020de4d35af086b3e911fd26a6699fb79
WENDYBAQUE <BAQUE.WENDY@gmail.com>
2022-05-02 16:48:06 +0200

    Add svg files to phone, mail, linkedin and github

--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -2,6 +2,21 @@ import React from "react";
 import "./contact.css";
 
 function Contact() {
-  return <div />;
+  return (
+    <div className="contact">
+      <div className="contact-bg" />
+      <div className="contact-wrapper">
+        <div className="contact-left">
+          <h1 className="contact-title">Contact</h1>
+          <div className="contact-info">
+            <div className="contact-info-item">
+              <img src="" alt="" className="contact-icon" />
+            </div>
+          </div>
+        </div>
+        <div className="contact-right">RIGHT</div>
+      </div>
+    </div>
+  );
 }
 export default Contact;

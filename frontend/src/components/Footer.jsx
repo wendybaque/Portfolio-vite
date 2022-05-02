159983e89b9bb8aee619b41437bfbda09b9b2ded
WENDYBAQUE <BAQUE.WENDY@gmail.com>
2022-05-02 13:32:27 +0200

    Add footer style

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -2,6 +2,12 @@ import React from "react";
 import "./footer.css";
 
 function Footer() {
-  return <div />;
+  return (
+    <div className="footer-text">
+      <p>Copyright ©2022</p>
+      <p>Wendy Baqué - Développeuse web.</p>
+      <p className="legal-mentions">Mentions Légales</p>
+    </div>
+  );
 }
 export default Footer;

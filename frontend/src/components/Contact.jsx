3e14654caf5c0636bdcead42f7111d22891bca52
WENDYBAQUE <BAQUE.WENDY@gmail.com>
2022-05-03 05:41:23 +0200

    Add links and icons to contact component

--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 import "./contact.css";
+import mail from "../assets/mail.svg";
+import phone from "../assets/phone.svg";
+import githubsvg from "../assets/github.svg";
+import linkedinsvg from "../assets/linkedin.svg";
 
 function Contact() {
   return (
@@ -10,7 +14,22 @@ function Contact() {
           <h1 className="contact-title">Contact</h1>
           <div className="contact-info">
             <div className="contact-info-item">
-              <img src="" alt="" className="contact-icon" />
+              <img src={phone} alt="phone" className="contact-icon" /> 06 98 15
+              71 13
+            </div>
+            <div className="contact-info-item">
+              <img src={mail} alt="mail" className="contact-icon" />{" "}
+              baque.wendy@gmail.com
+            </div>
+            <div className="contact-info-item">
+              <img src={linkedinsvg} alt="linkedin" className="contact-icon" />{" "}
+              <a href="https://www.linkedin.com/in/wendy-baqu%C3%A9/">
+                Wendy Baqué{" "}
+              </a>
+            </div>
+            <div className="contact-info-item">
+              <img src={githubsvg} alt="github" className="contact-icon" />
+              <a href="https://github.com/wendybaque">wendybaque</a>
             </div>
           </div>
         </div>

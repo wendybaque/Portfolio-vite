33c94ad611e966a8fa983befcfa64a0048f8f129
Wendy Baqué <99959128+wendybaque@users.noreply.github.com>
2022-05-05 14:03:27 +0200

    Add images to project component

--- frontend/src/components/ProjectCard.jsx
+++ frontend/src/components/ProjectCard.jsx
@@ -2,8 +2,13 @@ import React from "react";
 import "./projectcard.css";
 
 import logoauteure from "../assets/logoauteure.png";
-// import comingsoon from "../assets/comingsoon.jpg";
-// import kodework from "../assets/GoFullPageKodework.png";
+import comingsoon from "../assets/comingsoon.jpg";
+// import hackathon from "../assets/hackathon.jpg";
+// import project from "../assets/project.jpg";
+// import resto from "../assets/reactpropstate.png";
+// import simpsons from "../assets/simpsonsquote.png";
+// import techchallenge from "../assets/techchallengewild.png";
+import kodework from "../assets/kodework.png";
 
 function ProjectCard() {
   return (
@@ -19,56 +24,73 @@ function ProjectCard() {
           </p>
           <div className="project-card-links">
             <div className="links-github">
-              <a href="https://github.com/wendybaque/site-auteure">
+              <a
+                href="https://github.com/wendybaque/site-auteure"
+                target="_blank"
+                rel="noreferrer"
+              >
                 Voir le code
               </a>
             </div>
             <div className="links-page">
-              <a href="efcaerc">Voir le site</a>
+              <a href="efcaerc" target="_blank" rel="noreferrer">
+                Voir le site
+              </a>
             </div>
           </div>
         </div>
       </div>
 
       <div className="project-card">
-        <div className="project-card-title">Site d&apos;auteure</div>
-        <div className="project-card-skills">React | Tailwindcss</div>
-        <img src={logoauteure} alt="logo" className="project-card-img" />
+        <div className="project-card-title">Projet de groupe 1 - KodeWork</div>
+        <div className="project-card-skills">HTML5 | CSS3 | Javascript</div>
+        <img src={kodework} alt="logo" className="project-card-img" />
         <div className="project-card-description">
           <p>
-            Création de mon site d&apos;auteure pour présenter mes romans et mon
-            parcours littéraire.
+            Site internet responsive permettant de mettre en relation
+            professionnels et particuliers pour trouver des lieux de coworking
+            partout en France
           </p>
           <div className="project-card-links">
             <div className="links-github">
-              <a href="https://github.com/wendybaque/site-auteure">
+              <a href="https://github.com/SAUCYCorentin/coworking-project1">
                 Voir le code
               </a>
             </div>
             <div className="links-page">
-              <a href="efcaerc">Voir le site</a>
+              <a href="https://saucycorentin.github.io/">Voir le site</a>
             </div>
           </div>
         </div>
       </div>
 
       <div className="project-card">
-        <div className="project-card-title">Site d&apos;auteure</div>
-        <div className="project-card-skills">React | Tailwindcss</div>
-        <img src={logoauteure} alt="logo" className="project-card-img" />
+        <div className="project-card-title">
+          Projet de groupe 2 - Mood, une application de choix
+        </div>
+        <div className="project-card-skills">React et Hooks</div>
+        <img src={comingsoon} alt="logo" className="project-card-img" />
         <div className="project-card-description">
           <p>
-            Création de mon site d&apos;auteure pour présenter mes romans et mon
-            parcours littéraire.
+            Site internet responsive permettant de fair une association entre
+            films et pizzas, si on ne sait pas quoi regarder ou si on hésite sur
+            la composition de notre pizza ! Un peu de fun pour tester en groupe
+            toutes les fonctionnalités qu&apos;offre React.
           </p>
           <div className="project-card-links">
             <div className="links-github">
-              <a href="https://github.com/wendybaque/site-auteure">
+              <a
+                href="https://github.com/WildCodeSchool/2022-03-RemoteJS-Erreur404-tortues-ninjas"
+                target="_blank"
+                rel="noreferrer"
+              >
                 Voir le code
               </a>
             </div>
             <div className="links-page">
-              <a href="efcaerc">Voir le site</a>
+              <a href="efcaerc" target="_blank" rel="noreferrer">
+                Voir le site
+              </a>
             </div>
           </div>
         </div>

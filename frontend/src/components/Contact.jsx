4144862deb4c865546069a62ba58359c7210bd01
Wendy Baqué <99959128+wendybaque@users.noreply.github.com>
2022-05-06 07:39:52 +0200

    Fix bugs in parameters from emailjs

--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -1,11 +1,38 @@
-import React from "react";
+import React, { useRef, useState } from "react";
+// eslint-disable-next-line import/no-extraneous-dependencies
+import emailjs from "emailjs-com";
+
 import "./contact.css";
+
 import mail from "../assets/mail.svg";
 import phone from "../assets/phone.svg";
 import githubsvg from "../assets/github.svg";
 import linkedinsvg from "../assets/linkedin.svg";
 
 function Contact() {
+  const formRef = useRef();
+  const [done, setDone] = useState(false);
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    emailjs
+      .sendForm(
+        "service_ijptha3",
+        "template_7xuccag",
+        formRef.current,
+        "-F7_Yn5YXZZOlGfoZ"
+      )
+      .then(
+        (result) => {
+          // eslint-disable-next-line no-restricted-syntax
+          console.log(result.text);
+          setDone(true);
+        },
+        (error) => {
+          // eslint-disable-next-line no-restricted-syntax
+          console.log(error.text);
+        }
+      );
+  };
   return (
     <div className="contact">
       <div className="contact-bg" />
@@ -44,12 +71,14 @@ function Contact() {
             contacter sur Linkedin ou via le formulaire ci-dessous. Je vous
             répondrai dans les plus brefs délais.
           </p>
-          <form>
+          <form ref={formRef} onSubmit={handleSubmit}>
             <input type="text" placeholder="Nom" name="user_name" />
             <input type="text" placeholder="E-mail" name="user_email" />
             <input type="text" placeholder="Sujet" name="user_subject" />
             <textarea rows="6" placeholder="Message" name="message" />
             <button type="submit"> Envoyer </button>
+            {done &&
+              "Merci pour votre message. Je vous répondrai au plus vite !"}
           </form>
         </div>
       </div>
